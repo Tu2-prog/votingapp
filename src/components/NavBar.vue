b6e754c2be180ed94d8a9aa8d90e6e884a9edfa0
Tu2-prog <anhtuduong.nguyen@outlook.de>
2023-12-31 16:04:37 +0100

    began cleaning up gunk and add a navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-app-bar
+      color="white accent-4"
+      dark
+    >
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Evoter</v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer app v-model="drawer" absolute clipped>
+      <v-list nav>
+        <v-list-item v-for="item in items" :key="item.title" :to="item.link">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue'
+
+
+export default defineComponent({
+  name: 'NavBar',
+  data: () => ({
+    drawer: false,
+    items: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard', link: '/about'},
+          { title: 'Photos', icon: 'mdi-image', link: '/about' },
+          { title: 'About', icon: 'mdi-help-box', link: '/about' },
+        ]
+  }),
+})
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
-  <hello-world />
+  <NavBar/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
 // Components
-import HelloWorld from '../components/HelloWorld.vue';
+import NavBar from '@/components/NavBar.vue';
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    HelloWorld,
-  },
+    NavBar,
+},
 });
 </script>
