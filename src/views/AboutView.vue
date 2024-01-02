7c2ded5cfd8a19943f5dc4ca3cb55650dd1392f5
Tu2-prog <anhtuduong.nguyen@outlook.de>
2024-01-02 18:46:28 +0100

    added home to navigation bar and added all the views neccessary here

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+    <NavBar/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+// Components
+import NavBar from '@/components/NavBar.vue';
+
+export default defineComponent ({
+    name: 'AboutView',
+    components : {
+        NavBar
+    }
+
+})
+</script>
